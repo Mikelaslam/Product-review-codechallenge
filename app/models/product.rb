a5b98cf20810c494d3a8971dcdf7c902bd5ddf99
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-01 02:18:47 +0300

    Average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,11 @@ class Product < ActiveRecord::Base
   Review.where(product: self).each do |review|
     puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
   end
+  end
+  
+  def average_rating
+  Review.where(product: self).average(:star_rating).to_f
 end
+
 
 end
