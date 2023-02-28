f7790a46783c0943a73eb529f126f9fb2425763a
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-01 02:21:23 +0300

    Favourite product

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
 
-    
+    def favorite_product
+  Product.joins(reviews: :user)
+         .where(users: { id: id })
+         .order('reviews.star_rating DESC')
+         .first
+end
+
+
+
 end
