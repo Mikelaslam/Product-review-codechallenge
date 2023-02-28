1840be71f70f3a4ecbc997b442c9a120226acec8
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-01 02:16:20 +0300

    Leaving review

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
 
+  def leave_review(user, star_rating, comment)
+  Review.create(user: user, star_rating: star_rating, comment: comment, product: self)
+  end
+
+  
+
+
 end
